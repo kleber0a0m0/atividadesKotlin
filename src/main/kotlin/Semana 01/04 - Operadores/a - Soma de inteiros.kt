ab8bfcc854095a6da8f35f510ed9d2a3ed49ddfa
Kléber Moreira <kleber0a0m@gmail.com>
2023-04-19 08:22:21 -0300

    Atividades de Funções Definidas pelo Usuário

--- src/main/kotlin/Semana 01/04 - Operadores/a - Soma de inteiros.kt
+++ src/main/kotlin/Semana 01/04 - Operadores/a - Soma de inteiros.kt
@@ -1,10 +1,10 @@
 /*
-Crie duas variáveis inteiras e faça a soma utilizando o operador +.
+Crie duas variáveis inteiras e faça a `Semana 04`.`1 - Funções`.soma utilizando o operador +.
  */
 
 fun main() {
     val numero1: Int = 10
     val numero2: Int = 5
     val soma: Int = numero1 + numero2
-    println("Resultado da soma: $soma")
+    println("Resultado da `Semana 04`.`1 - Funções`.soma: $soma")
 }
---
+++ src/main/kotlin/Semana 04/1 - Funções/1 - Função soma.kt
@@ -0,0 +1,12 @@
+/*
+1 - Crie uma função que receba dois números inteiros
+como argumentos e retorne a `Semana 04`.`1 - Funções`.soma dos mesmos.
+ */
+
+fun soma(num1: Int, num2: Int): Int {
+    return num1 + num2
+}
+
+fun main() {
+    println(soma(1, 5));
+}
