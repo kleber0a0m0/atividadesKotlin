d6efc9da2d1fd89bee9bb8f369a54a780e05f631
Kléber Moreira <kleber0a0m@gmail.com>
2023-04-25 09:14:06 -0300

    Tipos de argumento de função - Listas - Maps

--- src/main/kotlin/Semana 01/05 - Entrada, processamento e saída/1 - Digitar números e somar.kt
+++ src/main/kotlin/Semana 01/05 - Entrada, processamento e saída/1 - Digitar números e somar.kt
@@ -1,5 +1,5 @@
 /*
-Faça um programa que peça ao usuário para digitar dois números e mostre a `Semana 04`.`1 - Funções`.soma desses números.
+Faça um programa que peça ao usuário para digitar dois números e mostre a `Semana 04`.`1 - Funções`.`Semana 04`.`1 - Funções`.soma desses números.
  */
 
 fun main() {
@@ -8,5 +8,5 @@ fun main() {
     println("Digite o segundo número:")
     val numero2 = readLine()?.toInt() ?: 0
     val soma = numero1 + numero2
-    println("A `Semana 04`.`1 - Funções`.soma dos números é $soma")
+    println("A `Semana 04`.`1 - Funções`.`Semana 04`.`1 - Funções`.soma dos números é $soma")
 }
